4557041363eb96a0def8c4e545455cf0159b9dcf
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-26 16:10:12 +0530

    seting the questions1

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,26 @@
+export const Questions = [
+    {
+      prompt: "What is my name?",
+      optionA: "Moinuddin",
+      optionB: "Shagufta",
+      optionC: "Shayasta",
+      optionD: "Saif",
+      asnwer: "Shagufta",
+    },
+    {
+      prompt: "Which of this is not one of Naruto Characters",
+      optionA: "Itachi Uchiha",
+      optionB: "Kakashi Hatake",
+      optionC: "Naruto Uzumaki",
+      optionD: "Tomoe",
+      asnwer: "Tomoe",
+    },
+    {
+      prompt: "RM learned English by watching which of the following shows",
+      optionA: "The Office",
+      optionB: "FRIENDS",
+      optionC: "How I Met Your Mother",
+      optionD: "Orange Is The New Black",
+      asnwer: "FRIENDS",
+    },
+  ];
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import { useState } from 'react';
+import { Questions } from '../Helpers/QuestionBank';
 
 function Quiz() {
+    const [currQuestion, setCurrentQuestion] = useState(1);
     return (
-        <div>
-            <h1>Quiz</h1>
+        <div className = "Quiz">
+            <h1>{Questions[currQuestion].prompt}</h1>
+            <div className="options">
+                <button> {Questions[currQuestion].optionA} </button>
+                <button> {Questions[currQuestion].optionB} </button>
+                <button> {Questions[currQuestion].optionC} </button>
+                <button> {Questions[currQuestion].optionD} </button>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Quiz
